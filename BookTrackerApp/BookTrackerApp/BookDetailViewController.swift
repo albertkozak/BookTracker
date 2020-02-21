3986e4c7202dc6accd39a0859684caaafb24a8e1
Albert Kozak <albert.kozak@live.com>
2020-02-21 15:21:39 -0800

    Completed book detail view

--- BookTrackerApp/BookTrackerApp/BookDetailViewController.swift
+++ BookTrackerApp/BookTrackerApp/BookDetailViewController.swift
@@ -9,22 +9,29 @@
 import UIKit
 
 class BookDetailViewController: UIViewController {
+  
+  @IBOutlet weak var imageView: UIImageView!
+  @IBOutlet weak var titleLabel: UILabel!
+  @IBOutlet weak var authorLabel: UILabel!
+  @IBOutlet weak var isbnLabel: UILabel!
+  @IBOutlet weak var ratingLabel: UILabel!
+  @IBOutlet weak var notesLabel: UILabel!
+  
   var book: Book?
-    override func viewDidLoad() {
+  
+  override func viewDidLoad() {
     super.viewDidLoad()
-      self.title = book?.title
-        // Do any additional setup after loading the view.
-    }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    if let definedBook = book,
+      let url = URL(string : definedBook.coverImageURL),
+      let data = try? Data(contentsOf: url)
+    {
+      self.imageView.image = UIImage(data: data)
+      self.titleLabel.text = book?.title
+      self.authorLabel.text = book?.author
+      self.isbnLabel.text = book?.isbn
+      self.ratingLabel.text = String(book!.rating)
+      self.notesLabel.text = book?.notes
     }
-    */
-
+  }
 }
